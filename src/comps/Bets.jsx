969b19eecf5ac1dee7c46c56d01ceb955ec88c51
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-09-18 22:30:48 -0400

    started bets styling

--- src/comps/Bets.jsx
+++ src/comps/Bets.jsx
@@ -6,16 +6,20 @@ export default function Bets({ bets }) {
     // setBetState(bets)
     return (
         <div>
+            <h3 style={{ fontSize: '2rem' }}>Your bets</h3>
             {bets === undefined ? (
                 <p>You currently have no saved bets</p>
             ) : (
+                    <div className="bets_wrapper">
 
-                    bets.map((bet, id) => (
-                        <div className='bets_wrapper' key={id}>
-                            <p>{bet.opponent1} vs {bet.opponent2}</p>
-                            <p>Result: {bet.win_loss} </p>
-                        </div>
-                    ))
+                        {bets.map((bet, id) => (
+                            <div className='bets' key={id}>
+                                <p>{bet.opponent1} vs {bet.opponent2}</p>
+                                <p>Result: {bet.win_loss} </p>
+                            </div>
+                        ))}
+                    </div>
+
                 )}
         </div >
     )
